e11c31688240161dd92dd8552f330c0fb112aa37
JuanManuel <juan.manuel.montes.reyes@uabc.edu.mx>
2022-03-01 09:01:59 -0800

    actualizado ejercicio 2

--- Assets/Scripts/ProblemaPractica2.cs
+++ Assets/Scripts/ProblemaPractica2.cs
@@ -6,27 +6,22 @@ public class NewBehaviourScript : MonoBehaviour
 {
     // Start is called before the first frame update
 
-     public List <int[]> testCases = new List <int[]>();
-    public List <int[]> testExpectedResults = new List <int[]>();
     public int[] resultado = new int[2] {0,0};
+    public int[] input;
+     int target;
 
-    void Start()
-    {
-        testCases.Add(new int[]{8,1,2,2,3});
-        testExpectedResults.Add(new int[]{4,0,1,1,3});
-    }
+
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.K))
         {
-            for (int i = 0; i < testCases.Count; i++)
+            int[] test1 = SumaDos(input, target);
+            foreach (int n in test1)
             {
-                int[] result = NumbersLessThan(testCases[i]);
-                bool passedTest = CompareTestResult(result,testExpectedResults[i]);
-                Debug.Log($"Test {i}  :  {passedTest}");   
-            }            
+                Debug.Log(n);    
+            }
         }
     }
 
@@ -50,18 +45,4 @@ public class NewBehaviourScript : MonoBehaviour
     }
 
 
-
-
-
-
-
-    private bool CompareTestResult(int[] result, int[] expectedResult)
-    {
-        if(expectedResult.Length != result.Length) return false;
-        for (int i = 0; i < result.Length; i++)
-        {
-            if(result[i] != expectedResult[i]) return false;
-        }
-        return true;
-    }
 }
